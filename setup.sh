7fcbfa5bc7a297edd90956e3f31e2bbc2de46daa
Proxieru <165452030+Proxieru@users.noreply.github.com>
2024-12-17 23:34:34 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 apt install wine64
 dpkg --add-architecture i386 && apt-get update && apt-get install wine32
+wget https://dl.winehq.org/wine/wine-mono/9.4.0/wine-mono-9.4.0-x86.msi
 wine msiexec /i wine-mono-7.5.0-x86.msi
 wine msiexec /i wine_gecko-2.47.3-x86.msi
 wget https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks
