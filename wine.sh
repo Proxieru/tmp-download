19f702e9d895512a7f27a68242631c54209c6ab2
Proxieru <165452030+Proxieru@users.noreply.github.com>
2024-12-19 13:26:35 -0800

    Update wine.sh

--- wine.sh
+++ wine.sh
@@ -1,6 +1,2 @@
-wget https://dl.winehq.org/wine/source/9.0/wine-9.0.tar.xz
-tar -xvf wine-9.0.tar.xz
-cd wine-9.0/
-sudo ./configure 
-sudo ./configure --enable-win64
-sudo make && sudo make install
+./configure ./configure --enable-win64
+make && make install
