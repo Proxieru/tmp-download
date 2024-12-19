c7f70c2c2282ed2c191401d558016999a109b160
Proxieru <165452030+Proxieru@users.noreply.github.com>
2024-12-19 14:28:59 -0800

    Update wine.sh

--- wine.sh
+++ wine.sh
@@ -1,14 +1,1 @@
-#!/bin/bash
-
-# Add WineHQ repository key 
-mkdir -p /etc/apt/keyrings
-wget -qO - https://dl.winehq.org/wine-builds/debian/keyring/winehq.key | sudo gpg --no-default-keyring --keyring /etc/apt/keyrings/winehq.key --import 
-
-# Add WineHQ repository to sources list
-echo "deb [signed-by=/etc/apt/keyrings/winehq.key] http://dl.winehq.org/wine-builds/debian bullseye main" | sudo tee /etc/apt/sources.list.d/winehq-bullseye.list
-
-# Update package list
-apt update
-
-# Install Wine 9.0 (replace 'stable' with 'devel' for development version)
-apt install --install-recommends winehq-stable 
+install mesa-vulkan-drivers libglx-mesa0:i386 mesa-vulkan-drivers:i386 libgl1-mesa-dri:i386
