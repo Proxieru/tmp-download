2b645793c14659d86ae096cae5686e82c49acdea
Proxieru <165452030+Proxieru@users.noreply.github.com>
2025-01-02 13:18:27 -0800

    Update wine.sh

--- wine.sh
+++ wine.sh
@@ -1,1 +1,6 @@
-wget https://github.com/NationalSecurityAgency/ghidra/releases/download/Ghidra_11.2.1_build/ghidra_11.2.1_PUBLIC_20241105.zip
+sudo dpkg --add-architecture i386
+sudo mkdir -pm755 /etc/apt/keyrings
+sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
+sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/debian/dists/bullseye/winehq-bullseye.sources
+sudo apt update
+sudo apt install --install-recommends winehq-stable
