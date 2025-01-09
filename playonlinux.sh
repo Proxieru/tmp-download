e03de29ae10394cdc379d66f6fb7423562bd70a5
Proxieru <165452030+Proxieru@users.noreply.github.com>
2025-01-09 14:25:14 -0800

    Update playonlinux.sh

--- playonlinux.sh
+++ playonlinux.sh
@@ -1,5 +1,7 @@
-echo "deb http://archive.ubuntu.com/ubuntu wily main universe" | sudo tee /etc/apt/sources.list.d/wily-copies.list
-sudo apt update
+apt-get install software-properties-common
+sudo add-apt-repository ppa:nilarimogard/webupd8
+sudo apt-get update
+sudo apt-get install python-wxgtk2.8
 wget -q "http://deb.playonlinux.com/public.gpg" -O- | apt-key add -
 wget http://deb.playonlinux.com/playonlinux_stretch.list -O /etc/apt/sources.list.d/playonlinux.list
 apt-get update
