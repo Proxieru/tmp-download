2d8d95298bba002d6a5b21c2c3e2bab324a8d84f
Proxieru <165452030+Proxieru@users.noreply.github.com>
2025-01-09 14:42:26 -0800

    Delete playonlinux.sh

--- playonlinux.sh
+++
@@ -1,9 +0,0 @@
-apt-get install software-properties-common
-sudo add-apt-repository ppa:nilarimogard/webupd8
-sudo apt-get update
-sudo apt-get install python-wxgtk2.8
-wget -q "http://deb.playonlinux.com/public.gpg" -O- | apt-key add -
-wget http://deb.playonlinux.com/playonlinux_stretch.list -O /etc/apt/sources.list.d/playonlinux.list
-apt-get update
-apt-get install playonlinux
-rm playonlinux.sh
