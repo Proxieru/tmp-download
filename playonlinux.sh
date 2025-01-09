6e893b9197cd3ec39a5dcf980391b7690ea6d683
Proxieru <165452030+Proxieru@users.noreply.github.com>
2025-01-09 14:20:17 -0800

    Update playonlinux.sh

--- playonlinux.sh
+++ playonlinux.sh
@@ -1,3 +1,5 @@
+echo "deb http://archive.ubuntu.com/ubuntu wily main universe" | sudo tee /etc/apt/sources.list.d/wily-copies.list
+sudo apt update
 wget -q "http://deb.playonlinux.com/public.gpg" -O- | apt-key add -
 wget http://deb.playonlinux.com/playonlinux_stretch.list -O /etc/apt/sources.list.d/playonlinux.list
 apt-get update
