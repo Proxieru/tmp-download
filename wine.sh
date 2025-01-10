e7cb041cbe9be84c0d956d90474be0e71400fb7e
Proxieru <165452030+Proxieru@users.noreply.github.com>
2025-01-10 12:28:46 -0800

    Update wine.sh

--- wine.sh
+++ wine.sh
@@ -1,6 +1,5 @@
-sudo dpkg --add-architecture i386
-sudo mkdir -pm755 /etc/apt/keyrings
-sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
-sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/debian/dists/bullseye/winehq-bullseye.sources
-sudo apt update
-sudo apt install --install-recommends winehq-stable
+sudo apt-key adv --fetch-keys http://repos.codelite.org/CodeLite.asc
+sudo tee /etc/apt/sources.list.d/wxwidgets.list <<EOF
+deb http://repos.codelite.org/wx3.0.2/ubuntu/   $(lsb_release -sc) universe
+EOF
+sudo apt-get updat
