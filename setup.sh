20a2f0779464e98d1fc3c4273a8d29f5fbdb0b7e
Proxieru <165452030+Proxieru@users.noreply.github.com>
2025-01-26 00:09:56 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,33 +1,29 @@
-echo "Installing sudo.."
-apt install sudo
-echo "Installing nano.."
-apt install nano
-echo "Installing vscode.."
-wget -O code_latest_amd64.deb "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
-sudo dpkg -i code_latest_amd64.deb
-sudo apt-get install -f
-sudo dpkg --add-architecture i386
-echo "Installing latest version of wine.."
-sudo mkdir -pm755 /etc/apt/keyrings
-sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
-sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/debian/dists/bullseye/winehq-bullseye.sources
-sudo apt update
-sudo apt install --install-recommends winehq-stable
-echo "Installing wine 32 & enabling wine32"
-dpkg --add-architecture i386 && apt-get update && apt-get install wine32
-echo "Installing wine-mono & wine-gecko"
-mkdir "Installed Files"
-cd /../"Installed Files"
-wget https://dl.winehq.org/wine/wine-mono/9.4.0/wine-mono-9.4.0-x86.msi
-wine msiexec /i wine-mono-7.5.0-x86.msi
-wine msiexec /i wine_gecko-2.47.3-x86.msi
-wget https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks
-chmod +x winetricks
-./winetricks -q dotnet20
-wget https://github.com/Proxieru/tmp-download/raw/refs/heads/main/lazyshell.exe
-wget https://github.com/Proxieru/tmp-download/raw/refs/heads/main/SuperYoshiRPGv148.smc
-cd /../
-echo "Installation done! (script will now delete itself)"
-wine --version
-"(if not wine-9-0 or similar, wine installed incorrectly)"
+#!/bin/bash
+
+sudo apt-get install -yq sudo > /dev/null 2>&1
+sudo apt-get install -yq nano > /dev/null 2>&1
+wget -q -O code_latest_amd64.deb "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
+sudo dpkg -i code_latest_amd64.deb > /dev/null 2>&1
+sudo apt-get install -f -yq > /dev/null 2>&1
+sudo dpkg --add-architecture i386 > /dev/null 2>&1
+sudo mkdir -pm755 /etc/apt/keyrings > /dev/null 2>&1
+sudo wget -q -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
+sudo wget -q -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/debian/dists/bullseye/winehq-bullseye.sources
+sudo apt-get update -yq > /dev/null 2>&1
+sudo apt-get install -yq --install-recommends winehq-stable > /dev/null 2>&1
+dpkg --add-architecture i386 > /dev/null 2>&1
+sudo apt-get update -yq > /dev/null 2>&1
+sudo apt-get install -yq wine32 > /dev/null 2>&1
+mkdir "Installed Files" > /dev/null 2>&1
+cd "Installed Files"
+wget -q https://dl.winehq.org/wine/wine-mono/9.4.0/wine-mono-9.4.0-x86.msi
+wine msiexec /i wine-mono-7.5.0-x86.msi /quiet > /dev/null 2>&1
+wine msiexec /i wine_gecko-2.47.3-x86.msi /quiet > /dev/null 2>&1
+wget -q https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks
+chmod +x winetricks > /dev/null 2>&1
+./winetricks -q dotnet20 > /dev/null 2>&1
+wget -q https://github.com/Proxieru/tmp-download/raw/refs/heads/main/lazyshell.exe
+wget -q https://github.com/Proxieru/tmp-download/raw/refs/heads/main/SuperYoshiRPGv148.smc
+cd ..
+wine --version > /dev/null 2>&1
 rm -rf setup.sh
