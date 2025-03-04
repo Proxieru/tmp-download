d1584df847175a5debcd760b9097b3cc4b292c75
Proxieru <165452030+Proxieru@users.noreply.github.com>
2025-03-03 19:27:19 -0800

    Update script.py

--- script.py
+++ script.py
@@ -13,12 +13,17 @@ label = tk.Label(root, text="Press the button")
 label.pack()
 
 # Create a button that calls `on_button_click` when pressed
-button = tk.Button(root, text="Generate fake files", command=on_button_click)
-button = tk.Button(root, text="Generate fake files with custome size", command=on_button_click)
-button = tk.Button(root, text="View generated fake files", command=on_button_click)
-button = tk.Button(root, text="Delete all fake files", command=on_button_click)
-button = tk.Button(root, text="Quit", command=on_button_click)
-button.pack()
+genbutton = tk.Button(root, text="Generate fake files", command=on_button_click)
+gencustombutton = tk.Button(root, text="Generate fake files with custome size", command=on_button_click)
+genviewbutton = tk.Button(root, text="View generated fake files", command=on_button_click)
+gendeletebutton = tk.Button(root, text="Delete all fake files", command=on_button_click)
+quitbutton = tk.Button(root, text="Quit", command=on_button_click)
+
+genbutton.pack()
+gencustombutton.pack()
+genviewbutton.pack()
+gendeletebutton.pack()
+quitbutton.pack()
 
 # Run the Tkinter event loop
 root.mainloop()
