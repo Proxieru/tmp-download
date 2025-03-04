d1bce14786826a6c8e440daaa1f52614f556e333
Proxieru <165452030+Proxieru@users.noreply.github.com>
2025-03-03 19:20:31 -0800

    Update script.py

--- script.py
+++ script.py
@@ -6,14 +6,18 @@ def on_button_click():
 
 
 root = tk.Tk()
-root.title("Button Example")
+root.title("Bloatinator")
 
 # Create a label
 label = tk.Label(root, text="Press the button")
 label.pack()
 
 # Create a button that calls `on_button_click` when pressed
-button = tk.Button(root, text="Click Me", command=on_button_click)
+button = tk.Button(root, text="Generate fake files", command=on_button_click)
+button = tk.Button(root, text="Generate fake files with custome size", command=on_button_click)
+button = tk.Button(root, text="View generated fake files", command=on_button_click)
+button = tk.Button(root, text="Delete all fake files", command=on_button_click)
+button = tk.Button(root, text="Quit", command=on_button_click)
 button.pack()
 
 # Run the Tkinter event loop
